311a548e0ef2d5043298b449f0b3a5768c7589a1
joao-ressel <joao.resselti@gmail.com>
2024-08-16 09:59:04 -0500

    resolução do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,21 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +29,6 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
     }
 }
